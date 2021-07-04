f39fe6ac58922b86326f32fa45968f2b1b1498fb
SFOX73 <alejandrozapata73@gmail.com>
2021-07-04 12:06:29 -0300

    e
    subiendo a githubPages

---
+++ script.js
@@ -0,0 +1,103 @@
+/*
+console.log('probando archivo')
+
+//Operadores...
+//  ( )...
+// Operadores de incremento / decremento
+
+var numero1 = 0
+
+console.log(numero1)
+
+numero1++
+numero1++
+numero1++
+numero1++
+numero1++
+
+//constantes 
+
+const PI = 3.1416
+
+var nombreOtraVariableVar = 5
+
+var nombreOtraVariableVar = 15
+
+console.log(nombreOtraVariableVar)
+
+let nombreOtraVariableLet = 10
+
+//let nombreOtraVariableLet = 16
+
+nombreOtraVariableLet = 18 // sobreescribiendo el valor
+
+console.log(nombreOtraVariableLet)
+
+//alert('Probando un alert')
+
+/*let nombreUsuario = prompt('Ingrese su nombre')
+
+nombreUsuario = 'Debora'
+
+nombreUsuario = 'Marina'
+
+const nombreConstante = prompt('ingrese PI')
+
+//nombreConstante = 3.26
+
+console.log(nombreUsuario)
+
+console.log(nombreConstante)
+
+*/
+/*
+//Tipos Booleanos ..true  .. false  ... binarios = 0  1
+// <  menor  <=... > mayor  <=.. == igual... != distinto  !negacion
+console.log(!5<6) // es verdadero
+console.log(!false) // es verdadero
+
+var numeroA = parseInt(prompt('ingrese un numero'))
+
+//numeroA = parseInt(numeroA) // parseFloat ( numeroA) // Number(numeroA)
+
+
+var numeroB = prompt('ingrese otro numero')
+
+numeroB = parseInt(numeroB)
+
+
+console.log(numeroA)
+console.log(numeroB)
+console.log('Resultado de la suma es: ' + (numeroA+numeroB))
+//convertir... parsear un valor
+console.log(1+true)
+
+//condicionales....
+//tomar de
+//
+
+*/
+
+if(false) {
+    console.log('hola mundo')
+}
+else {
+    console.log('no mundo')
+}
+
+
+if(3<4){
+    console.log('hola compuesto')
+}
+else if(3<5){
+    console.log('tambine')
+}
+else {
+    console.log('ninguno')
+}
+
+
+
+
+
+
